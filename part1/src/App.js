e1e2c12b63acb1cd2d086dd42708d24107533c7e
Alex Campbell <alexbe.campbell@gmail.com>
2021-03-22 15:50:23 -0500

    added Header/Footer, installed react router dom

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Header from '../src/Components/Header';
 const App = () => (
   <div>
+    <Header />
     <p>Hello world lo23</p>
   </div>
 )
---
+++ part1/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="App-header">Welcome</h1>
+        <nav>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Header;
