c2fcf01c60a497b5539be23fc6507f366be337a9
Alex Campbell <alexbe.campbell@gmail.com>
2021-03-22 16:04:20 -0500

    imported CSS styling using flex boxes with toggle open and closed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,60 @@
 import React from 'react'
 import Header from '../src/Components/Header';
+import './App.css';
+
+
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', toggleActive)
+);
+
 const App = () => (
+
+  
   <div>
     <Header />
     <p>Hello world lo23</p>
+    <div class="panels">
+      <div class="panel panel1">
+        <p>Hey</p>
+        <p>Let's</p>
+        <p>Dance</p>
+      </div>
+      <div class="panel panel2">
+        <p>Give</p>
+        <p>Take</p>
+        <p>Receive</p>
+      </div>
+      <div class="panel panel3">
+        <p>Experience</p>
+        <p>It</p>
+        <p>Today</p>
+      </div>
+      <div class="panel panel4">
+        <p>Give</p>
+        <p>All</p>
+        <p>You can</p>
+      </div>
+      <div class="panel panel5">
+        <p>Life</p>
+        <p>In</p>
+        <p>Motion</p>
+      </div>
+    </div>
   </div>
 )
 
